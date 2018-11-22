8282f362b9410116796e715e8fd0818033efe7a7
Alex <kushnir@performit.net>
2018-11-22 16:56:15 +0100

    End of Section 9

---
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+// Simple way of sending token within a header, but it doesnt work onInit, because in that moment token is not ready
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     'Authorization': 'Bearer' + ' ' + localStorage.getItem('token')
+//   })
+// };
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  baseUrl = environment.apiUrl;
+
+constructor(private http: HttpClient) { }
+
+getUsers(): Observable<User[]> {
+  return this.http.get<User[]>(this.baseUrl + 'users');
+  // return this.http.get<User[]>(this.baseUrl + 'users', httpOptions); // Simple way of sending token within a header
+}
+
+getUser(id): Observable<User> {
+  return this.http.get<User>(this.baseUrl + 'users/' + id);
+  // return this.http.get<User>(this.baseUrl + 'user/' + id, httpOptions); // Simple way of sending token within a header
+}
+
+}
---
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { UserService } from 'src/app/_services/user.service';
+import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+  user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
+  constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+    // this.loadUser(); // Alternative way, but in this case the data is not availble during Init phase, only after retrieving it again.
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+    this.galleryImages = this.getImages();
+  }
+
+  getImages() {
+    const imageUrls = [];
+    for (let i = 0; i < this.user.photos.length; i++) {
+      imageUrls.push({
+        small: this.user.photos[i].url,
+        medium: this.user.photos[i].url,
+        big: this.user.photos[i].url,
+        description: this.user.photos[i].description
+      });
+    }
+    return imageUrls;
+  }
+
+  // members/4
+  // loadUser() {
+  //   this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
+  //     this.user = user;
+  //   }, error => {
+  //     this.alertify.error(error);
+  //   });
+  // }
+
+}
